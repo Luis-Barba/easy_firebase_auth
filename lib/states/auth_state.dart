bfff8c3ed426fb4cd48db3a3aedbf4e5be4225bc
Luis <lymtechx@gmail.com>
2021-08-12 17:10:02 +0200

    Fix apple auth

--- lib/states/auth_state.dart
+++ lib/states/auth_state.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
-import 'dart:developer';
+import 'dart:convert';
+import 'dart:developer' as $;
 import 'dart:io';
+import 'dart:math';
+import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
@@ -59,7 +61,7 @@ class AuthState extends ChangeNotifier {
         }
       }
 
-      log("Initial auth status $_authStatus", name: LOG_TITLE);
+      $.log("Initial auth status $_authStatus", name: LOG_TITLE);
 
       // Check Splash Screen remaining time
       var splashScreenRemainingTime = splashScreenDurationMillis -
@@ -109,7 +111,7 @@ class AuthState extends ChangeNotifier {
     if (user != null) {
       await changeName(name);
       _authStatus = AuthStatus.LOGGED;
-      log("Status $_authStatus", name: LOG_TITLE);
+      $.log("Status $_authStatus", name: LOG_TITLE);
 
       await actionsAfterLogIn?.call(AuthMethod.EMAIL, firebaseUser);
     }
@@ -155,11 +157,11 @@ class AuthState extends ChangeNotifier {
     if (user != null) {
       // Successful Login
       _authStatus = AuthStatus.LOGGED;
-      log("Status $_authStatus", name: LOG_TITLE);
+      $.log("Status $_authStatus", name: LOG_TITLE);
 
       // Check Zombie
       if (wasAnonymous && previousUid != null) {
-        log("Zombie: $previousUid", name: LOG_TITLE);
+        $.log("Zombie: $previousUid", name: LOG_TITLE);
         await onZombieGenerated?.call(previousUid);
       }
 
@@ -181,14 +183,14 @@ class AuthState extends ChangeNotifier {
 
     await _myFirebaseAuth.signOut();
     _authStatus = AuthStatus.NOT_LOGGED;
-    log("Status $_authStatus", name: LOG_TITLE);
+    $.log("Status $_authStatus", name: LOG_TITLE);
 
     if (autoSignInAnonymously && canReauthenticate) {
       await _signIn(AuthMethod.ANONYMOUS, shouldNotify: false);
     }
 
     if (wasAnonymous && previousUid != null) {
-      log("Zombie: $previousUid", name: LOG_TITLE);
+      $.log("Zombie: $previousUid", name: LOG_TITLE);
       await onZombieGenerated?.call(previousUid);
     }
 
@@ -284,19 +286,45 @@ class _MyFirebaseAuth {
     return null;
   }
 
+  /// Generates a cryptographically secure random nonce, to be included in a
+  /// credential request.
+  String _generateNonce([int length = 32]) {
+    final charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
+  }
+
+  /// Returns the sha256 hash of [input] in hex notation.
+  String _sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
   Future<User?> signInApple() async {
+    // To prevent replay attacks with the credential returned from Apple, we
+    // include a nonce in the credential request. When signing in with
+    // Firebase, the nonce in the id token returned by Apple, is expected to
+    // match the sha256 hash of `rawNonce`.
+    final rawNonce = _generateNonce();
+    final nonce = _sha256ofString(rawNonce);
+
     try {
       final appleIdCredential = await SignInWithApple.getAppleIDCredential(
         scopes: [
           AppleIDAuthorizationScopes.email,
           AppleIDAuthorizationScopes.fullName,
         ],
+        nonce: nonce
       );
 
       OAuthProvider oAuthProvider = new OAuthProvider("apple.com");
       final AuthCredential credential = oAuthProvider.credential(
         idToken: appleIdCredential.identityToken,
         accessToken: appleIdCredential.authorizationCode,
+        rawNonce: rawNonce
       );
 
       await _firebaseAuth.signInWithCredential(credential); //AuthResult
@@ -308,7 +336,7 @@ class _MyFirebaseAuth {
 
       return _firebaseAuth.currentUser;
     } catch (e) {
-      log("error with apple sign in", name: LOG_TITLE);
+      $.log("error with apple sign in", name: LOG_TITLE, error: e);
     }
     return null;
   }
